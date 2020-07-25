ca57a27ad61a9d21e7e55d8a076711d55e01c965
leye195 <leye195@naver.com>
2020-07-26 02:49:24 +0900

    수정

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import "./styles.scss";
 import _ from "lodash";
 import test from "./test.jpg";
 const component = () => {
-	const element = document.createElement("div");
-	element.innerHTML = _.join(["Hello", "webpack"], " ");
-	return element;
+  const element = document.createElement("div");
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  return element;
 };
 
 const init = () => {
-	document.body.appendChild(component());
+  document.body.appendChild(component());
 };
 init();
--- webpack.config.js
+++ webpack.config.js
@@ -4,66 +4,66 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-	mode: "none",
-	entry: "./src/index.js",
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist"),
-		publicPath: "/dist/",
-	},
-	devServer: {
-		port: 9000,
-		hot: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: "babel-loader",
-						options: {
-							presets: ["@babel/preset-env"],
-						},
-					},
-				],
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					{ loader: "css-loader" },
-					{ loader: "sass-loader", options: {} },
-				],
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg)$/i,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							outputPath: "images",
-							name: "[name].[ext]?[hash]",
-						},
-					},
-				],
-			},
-		],
-	},
-	resolve: {
-		extentions: ["*", ".js"], //확장자 없이 불러오도록 설정 가능
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "[name].css",
-			chunkFilename: "[id].css",
-		}),
-		new HtmlWebpackPlugin({
-			template: "index.html",
-			scriptLoading: "defer",
-		}),
-	],
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  devServer: {
+    port: 9000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          { loader: "css-loader" },
+          { loader: "sass-loader", options: {} },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+              name: "[name].[ext]?[hash]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extentions: ["*", ".js"], //확장자 없이 불러오도록 설정 가능
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      scriptLoading: "defer",
+    }),
+  ],
 };
